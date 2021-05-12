1fab463c9525d4fce41d4f7611d026ddeebdd0ed
itss-c <mahmoud@itss-c.com>
2021-05-12 05:18:30 +0200

    aksa on the hard 2

---
+++ alquds_in_heart/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Ahmed Amen.
+
+
+{
+    "name":"Alquds In The Heart",
+    "summary":"Alquds In The Heart",
+'description': """
+    *   القدس عاصمة فلسطين 
+    """,
+    "version":"11.2.0.1",
+    "category":"General",
+    "author":"Ahmed Amen",
+    'license': 'LGPL-3',
+    "application":False,
+    "installable":True,
+    "depends":['purchase','base'],
+    "data":[
+        # "security/purchase_security.xml",
+        # "views/purchase_view.xml",
+    ],
+    'images': ['static/description/logo.png'],
+    "demo":[
+    ],
+}
---
+++ alquds_in_heart/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Ahmed Amen.
+
+
+from . import purchase
